1bef908e527671b31bc98c86bde5c0114bc4ad05
Mahmoud Magdy <115575025+mohmagdy214@users.noreply.github.com>
2023-11-04 14:12:14 +0200

    add screenshot taker

---
+++ Screenshot Taker/import pyautogui.py
@@ -0,0 +1,19 @@
+import pyautogui
+import tkinter as tk
+from tkinter.filedialog import asksaveasfilename
+
+root = tk.Tk()
+window = tk.Canvas(root, width=200, height=200)
+window.pack()
+
+
+def take_ss():
+    screen_shot = pyautogui.screenshot()
+    save_path = asksaveasfilename(defaultextension='.png')
+    screen_shot.save(save_path + "_screenshot.png")
+
+
+ss_button = tk.Button(text="TAKE SCREENSHOT", command=take_ss, font=15)
+window.create_window(100, 100, window=ss_button)
+
+root.mainloop()
--- number2words.py
+++ number2words.py
@@ -1,3 +1,3 @@
 from num2words import num2words
- 
+
 print(num2words(int(input("Enter Your number: "))))
